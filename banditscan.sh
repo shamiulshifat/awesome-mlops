ae3292d99a0d1d48534b93c419c1045fc9a16cb7
Shamiul Islam Shifat <36572440+shamiulshifat@users.noreply.github.com>
2022-10-27 11:37:27 +0600

    Update banditscan.sh

--- banditscan.sh
+++ banditscan.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-SRC_DIRECTORY="$(pwd)"
-REPORT_DIRECTORY="$SRC_DIRECTORY/report"
-
-if [ ! -d "$REPORT_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
-    mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
-fi
-
 # Make sure we are using the latest version
 docker pull cytopia/bandit:latest
 
 docker run --rm \
     --volume $(pwd):/data \
-    --volume "$REPORT_DIRECTORY":/data \
     cytopia/bandit:latest \
     -r /data
